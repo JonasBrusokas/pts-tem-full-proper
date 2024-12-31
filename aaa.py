614e502330e8b3e4bb50c1a6d54ffda952d19e66
Jonas Brusokas <jonas@flexshape.dk>
2024-12-31 20:32:43 +0100

    Remove test mode

--- aaa.py
+++ aaa.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     # %%
     NUM_WORKERS = 0
-    TEST_MODE = True
+    TEST_MODE = False
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
